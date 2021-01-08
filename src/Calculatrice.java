19ab181441119c954b91b64d4d0dd12ae4b12237
LANNEY Remi <r.lanney@ics-nice.com>
2021-01-08 11:47:03 +0100

    ajout commentaire + println

--- src/Calculatrice.java
+++ src/Calculatrice.java
@@ -4,8 +4,10 @@ public class Calculatrice {
 	public static void main(String[] args){
 		SwingUtilities.invokeLater(new Runnable(){
 			public void run(){
+				// Creation de ma fenetre
 				CalculatriceFenetre fenetre = new CalculatriceFenetre();
 				fenetre.setVisible(true);
+				println("test");
 			}
 		});
 	}
