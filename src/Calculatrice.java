f62f0006fe2f314af45c31ef2ee541d2e5cc9948
LANNEY Remi <r.lanney@ics-nice.com>
2021-01-08 11:57:06 +0100

    modif dev

--- src/Calculatrice.java
+++ src/Calculatrice.java
@@ -6,7 +6,8 @@ public class Calculatrice {
 			public void run(){
 				// Creation de ma fenetre
 				CalculatriceFenetre fenetre = new CalculatriceFenetre();
-				fenetre.setVisible(true);
+//				fenetre.setVisible(true);
+				println("test");
 			}
 		});
 	}
